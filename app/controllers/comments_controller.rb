2a9c24540efe8dff0ae581c3a284918748466587
Sarah K Shultis <skshultis@Sks-MacBook-Air.local>
2016-08-11 11:52:54 -0400

    so close

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,9 +7,16 @@ class CommentsController < ApplicationController
 
     def create
       @kardashian = Kardashian.find(params[:kardashian_id])
-      @comment = @kardashian.comments.create!(comment_params.merge(user: current_user))
-      redirect_to kardashian_path(@kardashian)
+      @comment = @kardashian.comments.new(comment_params.merge(user: current_user))
+      if current_user
+        @comment.save
+        redirect_to kardashian_path(@kardashian)
+      else
+        flash[:alert] = "sign in or create an account, slore!"
+        redirect_to new_user_session_path
+      end
     end
+
 
     def show
       @comment = Comment.find(params[:id])
---
+++ planning/user_stories.md
@@ -0,0 +1,6 @@
+1. As a user, I want to be able to make rude, snide remarks about anything related to the Kardashians.
+2. As a user, I want to be able to see my comments and others in the app.
+3. As a user, I want to be able to log in to the app.
+4. As a user, I want to delete comments if I regret posting them.
+5. As a user, I want to be reminded of how much I hate the Kardashians in order to help me write better and ruder comments at each visit.
+6. As a user, I want to delete comments that are inappropriate, or gross, or, like, mean, if I think they are.
