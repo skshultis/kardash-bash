44517f16776702b63c1c2876fe9af3f6fa3a8b18
Sarah K Shultis <skshultis@Sks-MacBook-Air.local>
2016-09-20 16:58:48 -0400

    edited seed data to correct quotations, minor css changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 Kardashian.destroy_all
 Comment.destroy_all
 
-kim = Kardashian.create ({name: "Kim", quote: "'I'm kind of a down ass b*tch.''", photo: "http://i.imgur.com/yNH6q7P.jpg"})
+kim = Kardashian.create ({name: "Kim", quote: "'I'm kind of a down ass b*tch.'", photo: "http://i.imgur.com/yNH6q7P.jpg"})
 
-kourt = Kardashian.create ({name: "Kourtney", quote: "'That is so embarrassing for your life and your soul.''", photo: "http://i.imgur.com/0Mgvkyz.jpg"})
+kourt = Kardashian.create ({name: "Kourtney", quote: "'That is so embarrassing for your life and your soul.'", photo: "http://i.imgur.com/0Mgvkyz.jpg"})
 
 khloe = Kardashian.create ({name: "Khloe", quote: "'Are you going to be a boring whore your whole life?''", photo: "http://i.imgur.com/o3Mf4Cf.jpg"})
 
-kylie = Kardashian.create ({name: "Kylie", quote: "'I will hit you in the throat.''", photo: "http://i.imgur.com/gBO5o2C.jpg"})
+kylie = Kardashian.create ({name: "Kylie", quote: "'I will hit you in the throat.'", photo: "http://i.imgur.com/gBO5o2C.jpg"})
 
-kendall = Kardashian.create ({name: "Kendall", quote: "'I've fallen off a horse five times.''", photo: "http://i.imgur.com/bR5S41H.jpg"})
+kendall = Kardashian.create ({name: "Kendall", quote: "'I've fallen off a horse five times.'", photo: "http://i.imgur.com/bR5S41H.jpg"})
 
 Comment.create ({text: "Kimye is so ugh I literally just can't.", name: "Jessica"})
 Comment.create ({text: "Justin and now love potion? Kourtney is dumber than ever.", name: "Brittany"})
